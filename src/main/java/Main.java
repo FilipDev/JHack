7709e3c729664b596686482bcc395064fcf6bd07
FilipDev <thespherret@gmail.com>
2014-12-22 15:22:26 -0500

    Moved .travis.yml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,9 @@
 import me.pauzen.jhack.misc.Pointer;
 import me.pauzen.jhack.objects.Objects;
-import me.pauzen.jhack.objects.memory.MemoryReader;
-import me.pauzen.jhack.objects.memory.implementations.MemoryModifierFactory;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
-        Thread runnable = new Test();
-
-        MemoryReader memoryModifier = MemoryModifierFactory.newModifier(runnable);
-        runnable.start();
-        //Objects.printInternals(runnable);
-        Thread.sleep(250L);
-        System.out.println("asdfasdf " + Objects.fromAddress(address));
     }
 
     public static long address = 0;
